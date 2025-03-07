858b0ef95ca7f5626e65efe91c52d7bfbc6e3b5c
sandip12321 <shri730182@gmail.com>
2025-03-07 09:25:34 +0530

    Add files via upload

---
+++ sumofthenumber.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int add(int);
+int add(int n, int s)
+{
+	printf("Enter the element in the array:");s
+	for(int i,s=0;i<n-1;i++)
+	{
+		sum=sum+i;
+		}
+		printf("%d",sum);	
+		return 0;
+		
+}
+int main()
+{
+	printf("Enter a number:");
+	scanf("%d",&n);
+	add(n);
+	return 0;
+}
